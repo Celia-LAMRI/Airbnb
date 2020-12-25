0be7b9f4b8318f6342dfa5308eb9135a8d727923
Celia <celia.lamri@yahoo.com>
2020-12-25 21:59:57 +0100

    mon projet

---
+++ test.java
@@ -0,0 +1,23 @@
+package airBnb;
+
+import java.util.Arrays;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		
+		//Voyageur v= new Voyageur("1","LAMRI","Celia",23,"femme");
+		Hote h= new Hote("2","LACHI","Yamina",22,"femme");
+		Appartement app= new Appartement("chiraton",24,2,1,70.50,1);
+		
+		
+		//System.out.println(v.toString());
+		System.out.println(h);
+		
+		
+		
+
+	}
+
+}
